81371228d2313c0a2e416f93502e5035f41ae9e7
farid-fari <7dare@outlook.com>
2017-03-24 21:28:27 +0100

    ajout du modèle SIR

--- epidemics/branching.py
+++ epidemics/branching.py
@@ -14,7 +14,7 @@ people = list(range(n))
 
 sick = [0]*n
 full = [1]*n
-sick[rand.randint(0,n-1)] = 1
+sick[rand.randint(0, n-1)] = 1
 
 graph = nx.Graph()
 graph.add_nodes_from(people)
@@ -24,36 +24,36 @@ previous = [p for p in people if sick[p]]
 u = []
 
 for m in range(10):
-    next = []
+    next_wave = []
     u.append(len(previous))
     for i in previous:
         j = 0
         while j < k:
             if sick == full:
                 j = k
-            t = rand.randint(0,n-1)
+            t = rand.randint(0, n-1)
             if not sick[t]:
                 j += 1
                 if rand.random() < p:
                     graph.add_edge(i, t, trans=1)
                     sick[t] = 1
-                    next.append(t)
+                    next_wave.append(t)
                 else:
                     graph.add_edge(i, t, trans=0)
-    previous = next
+    previous = next_wave
 
-nx.draw_circular(graph, 
-    node_color=sick,
-    cmap=mtpl.cm.get_cmap(name="autumn"),
-    vmin=0,
-    vmax=1,
-    with_labels=True,
-    edge_color=[1-graph.edge[t[0]][t[1]]['trans'] for t in graph.edges()],
-    edge_cmap=mtpl.cm.get_cmap(name="winter"),
-    edge_vmin=0,
-    edge_vmax=1,
-    linewidths=0.3,
-    width=80/n)
+nx.draw_circular(graph,
+                 node_color=sick,
+                 cmap=mtpl.cm.get_cmap(name="autumn"),
+                 vmin=0,
+                 vmax=1,
+                 with_labels=True,
+                 edge_color=[1-graph.edge[t[0]][t[1]]['trans'] for t in graph.edges()],
+                 edge_cmap=mtpl.cm.get_cmap(name="winter"),
+                 edge_vmin=0,
+                 edge_vmax=1,
+                 linewidths=0.2,
+                 width=80/n)
 
 mtpl.pyplot.show()
 
