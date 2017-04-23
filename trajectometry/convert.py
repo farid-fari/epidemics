11233f50242d87b924c84a76afcf42c8bc10c560
farid-fari <7dare@outlook.com>
2017-04-23 12:49:14 +0200

    conversion BDD et amélioration interface

---
+++ trajectometry/convert.py
@@ -0,0 +1,24 @@
+''' Outil permettant de convertir le format des données de trajectométrie.'''
+
+import sqlite3 as sq
+
+ancien = sq.connect('trajecto.db')
+nouveau = sq.connect('trajecto_nouv.db')
+ca = ancien.cursor()
+cn = nouveau.cursor()
+
+cn.execute("CREATE TABLE Personnes (cle BIGINT PRIMARY KEY, secteur INTEGER, age INTEGER, redressement FLOAT, occupation INTEGER)")
+cn.execute("CREATE TABLE Positions (id INTEGER PRIMARY KEY AUTOINCREMENT, cle BIGINT, heure INTEGER, endroit INTEGER, FOREIGN KEY(cle) REFERENCES Personnes(cle))")
+
+for k in ca.execute("SELECT * FROM donnees"):
+    cn.execute("INSERT INTO Personnes (cle, secteur, age, redressement, occupation) VALUES (?,?,?,?,?)", (k[0], k[1], k[2], k[3], k[4]))
+    heures = [int(x[0]) for x in ca.description[5:]]
+    for i, j in enumerate(k[5:]):
+        cn.execute("INSERT INTO Positions (cle, heure, endroit) VALUES (?,?,?)", (k[0], heures[i], j))
+
+ancien.commit()
+nouveau.commit()
+ca.close()
+cn.close()
+ancien.close()
+nouveau.close()
--- trajectometry/interface.py
+++ trajectometry/interface.py
@@ -6,15 +6,14 @@ import sqlite3 as sq
 import warnings
 
 class Person:
-    def __init__(self, cursor, cle=None, tablename="personnes"):
+    def __init__(self, cursor, cle=None):
         if cle is None:
-            cursor.execute("SELECT * FROM %s LIMIT 1" % tablename)
+            cursor.execute("SELECT * FROM Personnes LIMIT 1")
         else:
-            cursor.execute("SELECT * FROM %s WHERE cle = ? LIMIT 1" % tablename, (cle,))
+            cursor.execute("SELECT * FROM Personnes WHERE cle = ? LIMIT 1", (cle,))
         data = cursor.fetchone()
         if data is None:
             raise LookupError
-
         self.cle = data[0]
         if not cle is None and self.cle != cle:
             warnings.warn("La clé ne correspond pas à celle spécifiée.")
@@ -22,15 +21,17 @@ class Person:
         self.age = data[2]
         self.redressement = data[3]
         self.occupation = data[4]
-        self.times = data[5:]
+        times = []
+        for h in cursor.execute("SELECT endroit FROM Positions WHERE cle=? ORDER BY heure ASC", (self.cle,)):
+            times.append(h[0])
+        self.times = times[5:]
 
     def __str__(self):
         return "id=" + str(self.cle)
 
 if __name__ == "__main__":
-    _conn = sq.connect('trajecto.db')
+    _conn = sq.connect('trajecto_nouv.db')
     _curs = _conn.cursor()
     p = Person(_curs)
     _curs.close()
     _conn.close()
-
